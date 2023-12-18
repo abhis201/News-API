218cbaf0a77ad872dc9e406291dfd2a56380817b
singhAbhiPNW <sing1290@pnw.edu>
2023-12-18 15:19:11 -0600

    db changes

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/NewsApi",{
+mongoose.connect("mongodb+srv://abhis201:Zj4UnUrNz3hUg0NL@coursify.xizinwi.mongodb.net/NewsApi",{
     useNewUrlParser : true,
     useUnifiedTopology: true
 },()=>{
